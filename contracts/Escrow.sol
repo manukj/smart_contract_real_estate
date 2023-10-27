59789098d35c0697ea5cfeda2d899b5f1db164af
Manu <manu1998kj@gmail.com>
2023-10-27 08:10:51 +0100

    added constraints

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -9,23 +9,60 @@ contract Escrow{
 
     address public nftAddress;
     uint256 public nftID;
+    uint256 public purchaseAmount;
+    uint256 public escrowAmount;
     address payable public seller;
     address payable public buyer;
+    address public inspector;
+    address public lender;
+  
 
     constructor(
                 address _nftAddress,
                 uint256 _nftID,
+                uint256 _purchaseAmount,
+                uint256 _escrowAmount,
                 address payable _seller,
-                address payable _buyer
+                address payable _buyer,
+                address  _inspector,
+                address  _lender
                 ){
         nftAddress = _nftAddress;
         nftID = _nftID;
+        purchaseAmount = _purchaseAmount;
+        escrowAmount = _escrowAmount;
         seller = _seller;
         buyer = _buyer;
+        inspector = _inspector;
+        lender = _lender;
     }
 
     function finalizeSale() public {
         IERC721(nftAddress).transferFrom(seller, buyer, nftID);
     }
+
+
+    function getBalance() public view returns(uint) {
+        return address(this).balance;
+    }
+
+
+    function depositeEarnest() public payable onlyBuyer {
+        require(msg.value >= escrowAmount,"Can not deposite money ");
+    }
     
+    modifier onlyBuyer(){
+        require(msg.sender == buyer,"Only buyer is allowed to call this function");
+        _;
+    } 
+    
+    modifier onlySeller(){
+        require(msg.sender == seller,"Only Seller is allowed to call this function");
+        _;
+    }  
+    
+    modifier onlyInspector(){
+        require(msg.sender == inspector,"Only Inspector is allowed to call this function");
+        _;
+    }
 }
--- test/RealEstate.cjs
+++ test/RealEstate.cjs
@@ -1,12 +1,18 @@
 const { expect } = require('chai');
 const { ethers } = require('hardhat');
 
+const tokens = (n)=>{
+  return ethers.parseUnits(n.toString(),'ether')
+}
 
+const ether = tokens;
 
 describe('RealEstate', () => {
   let realEstate, escrow
   let deployer, seller, buyer, inspector, lender
   let nftId = 1
+  let purchaseAmount = ether(100)
+  let escrowAmount = ether(20)
 
 
   beforeEach(async () => {
@@ -26,10 +32,15 @@ describe('RealEstate', () => {
     realEstate = await RealEstate.deploy()
     
     escrow = await Escrow.deploy(
-      realEstate,
-      nftId,
-      seller.address,
-      buyer.address)
+        realEstate,
+        nftId,
+        purchaseAmount,
+        escrowAmount,
+        seller.address,
+        buyer.address,
+        inspector.address,
+        lender.address
+      )
 
     //approve the transcation from seller 
     transcation = await realEstate.connect(seller).approve(escrow,nftId);
@@ -37,9 +48,20 @@ describe('RealEstate', () => {
   })
   
   describe('deployment',()=>{
+    let transcation,balance
+
     it("test transfer",async()=>{
         //verify if the seller is the onwer of the nft 
         expect(await realEstate.ownerOf(nftId)).to.equal(seller.address)
+
+        //deposite the eschrow amount
+        transcation = await escrow.connect(buyer).depositeEarnest({value: escrowAmount})
+        await transcation.wait();
+
+        // check the balance 
+        balance = await escrow.getBalance()
+        var amount = ethers.parseEther('5000.0');
+        console.log(ethers.formatEther(balance))
 
         // //transfer the nft to buyer 
         transcation = await escrow.connect(buyer).finalizeSale();
